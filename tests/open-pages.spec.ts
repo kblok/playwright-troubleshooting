4e8b95316c4e84464bf1f31a3bbf42e27c1b75ff
Darío Kondratiuk <dariokondratiuk@gmail.com>
2023-06-16 15:42:24 -0300

    open pages test

---
+++ tests/open-pages.spec.ts
@@ -0,0 +1,74 @@
+import { test } from '@playwright/test';
+
+test('pdf in iframe', async ({ context }) => {
+  let page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+  page = await context.newPage();
+
+  await page.goto(
+    "https://storage.googleapis.com/mabl-prod-workspace-qui200igbmeqwir3uvig700us/execution-input/vj5h/email/Iu_MMK5HXi2FcEDamityaHZyeO4/email.html?GoogleAccessId=cloud-storage-signer@mabl-prod.iam.gserviceaccount.com&Expires=1687472483&Signature=mEJm%2FuEWg0dkr4AZDYF%2Fh890%2B3K8Lj7kjbhTNvVSLytvFBoYJdSridLrPNNsA4wJyvjrZCV6qoau6KTu%2BPMHzeGOAVWkNQiwWIuQpwFlEGIDx18bTNbRJHFyiJJvsY70bkFv7qX6ddrAWum96TvIa%2FHsDksYB8E0ltslgbRoZGJGvDB3IfNOQotEJivLA98e%2BWSXxOkTciXkrHZTGKgRg9Faug0jb5EIVDx70YyKBSwJctpMh4rTC5Z77%2F5VPEBqCSgWODzio0MGdiw%2BgzAAHgCyqOTXXleFOq5vO2Zr3QUpA9y6Yf3hPCK1r6MvGWU%2BHj0hmv%2B2TbHZmQsHFQVUVA%3D%3D",
+    { waitUntil: "load" }
+  );
+  await page.screenshot();
+});
